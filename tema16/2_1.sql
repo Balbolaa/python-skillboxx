d742e242940d4679329565fce202f6c59dc8e406
eyad balbola <excskully@yahoo.com>
2024-10-02 21:45:48 +0500

    Update 2_1.sql

--- tema16/2_1.sql
+++ tema16/2_1.sql
@@ -1,3 +1,17 @@
-SELECT customer.full_name, manager.full_name, purchase_amount, date FROM "order"
-LEFT JOIN customer ON "order".customer_id = customer.customer_id
-LEFT JOIN manager ON "order".manager_id = manager.manager_id
+
+SELECT 
+    customer.full_name,         -- The full name of the customer making the order
+    manager.full_name,          -- The full name of the manager overseeing the order
+    purchase_amount,            -- The total amount of the purchase in the order
+    date                        -- The date when the order was made
+FROM "order"                    -- From the "order" table which stores order data
+
+-- Left Join the "customer" table based on the customer_id
+-- Retrieves additional information from the customer table, specifically the full name
+LEFT JOIN customer 
+    ON "order".customer_id = customer.customer_id
+
+-- Left Join the "manager" table based on the manager_id
+-- Retrieves the manager's full name from the manager table
+LEFT JOIN manager 
+    ON "order".manager_id = manager.manager_id;
